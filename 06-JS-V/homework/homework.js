5345be7777a5dcf26e187cc7d364e274934bdf4d
GonzVerb22 <gonzatomaverb@hotmail.com>
2022-09-08 17:15:36 -0300

    08/09

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,22 +8,29 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-  function Usuario(opciones){
-    this.usuario = opciones.usuario,
-    this.nombre = opciones.nombre,
-    this.email = opciones.email,
-    this.password = opciones.password 	 
+  class Usuario {
+    constructor (opciones){
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
+      }
+    }
+  Usuario.prototype.saludar = function (){
+    return `Hola, mi nombre es ${this.nombre}`
   }
- Usuario.prototype.saludar = function(){
-  return "Hola, mi nombre es " + this.nombre
- }
-  
+  return Usuario
 }
+  
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function(){
+    return "Hello World!";
+  }
 }
 
 function agregarStringInvertida() {
@@ -32,9 +39,19 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+/* String.prototype.reverse = function(){
+  var palabraInvertida = "";
+for (var i = this.length - 1; i >= 0  ; i--) {
+  palabraInvertida += this[i];
+}
+return palabraInvertida;
+}*/
+String.prototype.reverse = function(){
+  return this.split("").reverse().join("");
+}
 }
 
-// ---------------------------------------------------------------------------//
+
   //Crea el constructor de la clase "Persona"
   //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
   //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
