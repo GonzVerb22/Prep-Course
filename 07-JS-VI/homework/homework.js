c451b887529ea7f120d921634f68ce69384b4129
GonzVerb22 <gonzatomaverb@hotmail.com>
2022-09-10 12:24:51 -0300

    10/09/22

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,24 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  let aux = nombre[0].toUpperCase();
+  let name = aux + nombre.slice (1);
+  return name;
+
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb (cb);
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb (n1,n2);
+
 }
 
 function sumarArray(numeros, cb) {
@@ -22,6 +29,11 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let total = 0
+ for(let i = 0; i < numeros.length; i++) {
+  total += numeros[i];
+ }
+ cb(total);
 }
 
 function forEach(array, cb) {
