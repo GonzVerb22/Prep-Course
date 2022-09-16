5da151ed0ab6550e3e48000a1a0af14fff202f75
GonzVerb22 <gonzatomaverb@hotmail.com>
2022-09-16 16:44:42 -0300

    terminado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -60,7 +60,19 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  
+    let palabrasSeparadas = str.split (" ");
+    let fraseInvertida = []
+    for(let i = 0; i < palabrasSeparadas.length; i++){
+      // let palabraInvertida = "";
+      // palabraInvertida= palabrasSeparadas[i].split("");
+      // palabraInvertida=palabraInvertida.reverse()
+      // palabraInvertida= palabraInvertida.join("");
+     fraseInvertida.push(palabrasSeparadas[i].split("").reverse().join(""))
+    }
+ return fraseInvertida.join(" ")
+}
+
 
 
 function capicua(numero){
@@ -68,6 +80,12 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let numeroString = numero.toString()
+  if (numeroString.split("").reverse().join("") !== numeroString){
+    return "No es capicua";
+  }else{
+    return "Es capicua";
+  }
 }
 
 
@@ -75,14 +93,33 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  let palabra = "";
+  for(let i = 0 ; i < cadena.length ; i++){
+    if(cadena [i] !== "a" && cadena [i] !== "b" && cadena[i] !== "c"){
+    palabra += cadena[i];
+    }
+  }
+  return palabra;
 }
-
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-}
+  do {
+    var cambios = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i].length > arr[i + 1].length) {
+      var store = arr[i + 1];
+      arr[i + 1] = arr[i];
+      arr[i]= store;
+      cambios = true;
+     }   
+    }
+  } while (cambios);
+  return arr;
+  }
+
 
 
 function buscoInterseccion(arreglo1, arreglo2){
@@ -91,6 +128,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+let nuevoArraglo = [];
+for (let i = 0; i < arreglo1.length ; i++){
+  let element = arreglo1[i];
+  for(let j = 0; j < arreglo2.length ; j++){
+    if (element === arreglo2[j]){
+      nuevoArraglo.push(element);
+    }
+  }
+}
+return nuevoArraglo;
 }
 
 
